53f59aeac6dd155b817884a833776d6fc9de5849
Jennings Liu <jenningsloy318@gmail.com>
2017-06-27 22:13:51 +0800

    add second example of chapter 6

---
+++ Exercises/principles_of_data_science/chapter6/descrete_radom_variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import random
+from matplotlib import pyplot as plt
+
+def random_variables_of_dice_roll():
+    return random.randint(1, 7)
+
+
+num_trials = range(100,10000,10)
+avgs=[]
+for num_trial  in num_trials:
+    trials=[]
+    for trial in range(num_trial):
+        trials.append(random_variables_of_dice_roll())
+    avgs.append(sum(trials)/float(num_trial))
+plt.plot(num_trials,avgs)
+plt.xlabel("Number of trials")
+plt.ylabel("Average")
+plt.show()
